d39176ce62ab017fbe8cc0a3c4fc10791fdb55ec
8008086655 <109746884+8008086655@users.noreply.github.com>
2022-09-06 14:56:09 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,10 +1,10 @@
-def call(ip, user, credID){
+def call(ip,user,credID){
   sshagent([credID]) {
- // copy war
- sh 'scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps/' 
- // stop tomcat
- sh 'ssh ${user}@172.31.40.103 /opt/tomcat9/bin/shutdown.sh'
- // start tomcat
- sh 'ssh ${user}@172.31.40.103 /opt/tomcat9/bin/startup.sh'
+     // copy war
+     sh 'scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps/' 
+     // stop tomcat
+     sh 'ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh'
+     // start tomcat
+     sh 'ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh'
   }
 }
